e37a68d6a0a74ad83fae862f790e3866a8e57bd5
nikimacm <nickymacm@gmail.com>
2022-03-30 11:58:39 +0000

    changed the context file as I will not have a delivery option in my bag. Instead I will have a discount on items over 300 euro and the patterns will be sent by email. Udated base.html and bag.html with changes made to context.py

--- bag/contexts.py
+++ bag/contexts.py
@@ -21,22 +21,22 @@ def bag_contents(request):
             'product': product,
         })
 
-    if total < settings.FREE_DELIVERY_THRESHOLD:
-        delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
-        free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
+    if total < settings.ORDER_DISCOUNT_THRESHOLD:
+        discount = total * Decimal(settings.ORDER_DISCOUNT_AMOUNT / 100)
+        order_discount_delta = settings.ORDER_DISCOUNT_THRESHOLD - total
     else:
-        delivery = 0
-        free_delivery_delta = 0
+        discount = 10
+        order_discount_delta = 0
     
-    grand_total = delivery + total
+    grand_total = discount + total
     
     context = {
         'bag_items': bag_items,
         'total': total,
         'product_count': product_count,
-        'delivery': delivery,
-        'free_delivery_delta': free_delivery_delta,
-        'free_delivery_threshold': settings.FREE_DELIVERY_THRESHOLD,
+        'discount': discount,
+        'order_discount_delta': order_discount_delta,
+        'order_discount_threshold': settings.ORDER_DISCOUNT_THRESHOLD,
         'grand_total': grand_total,
     }
 
