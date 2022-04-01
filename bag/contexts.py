dd4ac40a456c9d4fd9b3206d056eedaa2ee9ef02
nikimacm <nickymacm@gmail.com>
2022-04-01 12:23:34 +0000

    updated items by size in views, contexts and product_detail

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,7 +1,7 @@
 from decimal import Decimal
 from django.conf import settings
 from django.shortcuts import get_object_or_404
-from products.models import Product 
+from products.models import Product
 
 def bag_contents(request):
 
@@ -11,14 +11,26 @@ def bag_contents(request):
     bag = request.session.get('bag', {})
 
     for item_id, item_data in bag.items():
-        product = get_object_or_404(Product, pk=item_id)
-        total += item_data * product.price
-        product_count += item_data
-        bag_items.append({
-            'item_id': item_id,
-            'quantity': item_data,
-            'product': product,
-        })
+        if isinstance(item_data, int):
+            product = get_object_or_404(Product, pk=item_id)
+            total += item_data * product.price
+            product_count += item_data
+            bag_items.append({
+                'item_id': item_id,
+                'quantity': item_data,
+                'product': product,
+            })
+        else:
+            product = get_object_or_404(Product, pk=item_id)
+            for size, quantity in item_data['items_by_size'].items():
+                total += quantity * product.price
+                product_count += quantity
+                bag_items.append({
+                    'item_id': item_id,
+                    'quantity': item_data,
+                    'product': product,
+                    'size': size,
+                })
 
     if total < settings.ORDER_DISCOUNT_THRESHOLD:
         discount = total * Decimal(settings.ORDER_DISCOUNT_AMOUNT / 100)
