3fdb4c75f416e7b3bf060672ca33e8fc8d5b49cc
nikimacm <nickymacm@gmail.com>
2022-04-02 17:28:01 +0000

    added toast messages to views

--- bag/contexts.py
+++ bag/contexts.py
@@ -17,7 +17,7 @@ def bag_contents(request):
             product_count += item_data
             bag_items.append({
                 'item_id': item_id,
-                'quantity': quantity,
+                'quantity': item_data,
                 'product': product,
             })
         else:
@@ -27,7 +27,7 @@ def bag_contents(request):
                 product_count += quantity
                 bag_items.append({
                     'item_id': item_id,
-                    'quantity': item_data,
+                    'quantity': quantity,
                     'product': product,
                     'size': size,
                 })
