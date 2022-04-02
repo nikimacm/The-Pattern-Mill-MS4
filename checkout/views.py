6e4f567661a9de446340195d5e8baaff19b62869
nikimacm <nickymacm@gmail.com>
2022-04-02 19:24:49 +0000

    added js elements to the checkout form implementing stripe

--- checkout/views.py
+++ checkout/views.py
@@ -14,7 +14,7 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51KkC1GAjEPNREPT2lRaQ99sqDP896gFy8UH0IicC2QDwc8Tq24FSdrwjZ6eo7Eh49HXaFl1dnr5DbSnko3apN2EZ00FIeo2nW3',
+        'stripe_public_key': 'pk_test_0SMREd7Vdweb1MGRi8S0EycR00JVzSAs5O',
         'client_secret': 'test client secret',
     }
 
