5662e093262fb8590dbda716da9f17eeb7d5cd32
nikimacm <nickymacm@gmail.com>
2022-04-03 07:03:13 +0000

    added checkout functionality to stripe elements.js

--- the_pattern_mill/settings.py
+++ the_pattern_mill/settings.py
@@ -171,5 +171,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+# stripe
 ORDER_DISCOUNT_THRESHOLD = 300
 ORDER_DISCOUNT_AMOUNT = 10
+STRIPE_CURRENCY = 'usd'
+STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
