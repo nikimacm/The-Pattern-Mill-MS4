bebef5a1a304636122f2d4a07a18a9a1d85ee3f6
nikimacm <nickymacm@gmail.com>
2022-04-03 07:46:36 +0000

    added checkout success html

--- checkout/models.py
+++ checkout/models.py
@@ -34,7 +34,7 @@ class Order(models.Model):
         Update grand total each time a line item is added,
         accounting for delivery costs.
         """
-        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum']
+        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum'] or 0
         if self.order_total < settings.ORDER_DISCOUNT_THRESHOLD:
             self.delivery_cost = self.order_total * settings.ORDER_DISCOUNT_AMOUNT / 100
         else:
