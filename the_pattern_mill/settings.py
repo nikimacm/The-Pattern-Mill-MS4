8ff870b7b048d5a57d7e359b60db57bd75e6e2e1
nikimacm <nickymacm@gmail.com>
2022-04-03 22:07:40 +0000

    moved storages in settings to fix error

--- the_pattern_mill/settings.py
+++ the_pattern_mill/settings.py
@@ -45,10 +45,10 @@ INSTALLED_APPS = [
     'products',
     'bag',
     'checkout',
-    'storages',
 
     # Other
     'crispy_forms',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -174,6 +174,12 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
+    # Cache control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
+
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'the-pattern-mill-ms4'
     AWS_S3_REGION_NAME = 'eu-central-1'
