d301de7cd84836cf801a5f168e1b71a0fd5d7e82
nikimacm <nickymacm@gmail.com>
2022-05-04 10:02:43 +0000

    removed old products.json file to add replacement

--- the_pattern_mill/settings.py
+++ the_pattern_mill/settings.py
@@ -118,10 +118,12 @@ WSGI_APPLICATION = 'the_pattern_mill.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 if 'DATABASE_URL' in os.environ:
+    print("Loading postgres db ....")
     DATABASES = {
         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
 else:
+    print("Loading local db ....")
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
