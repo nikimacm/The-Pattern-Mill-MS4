d3b701b76192caeb731e57b96890553b2d7381bd
nikimacm <nickymacm@gmail.com>
2022-05-26 14:48:11 +0000

    added order hsitory to profiles/views, updated urls and checkput-success.html

--- profiles/views.py
+++ profiles/views.py
@@ -1,9 +1,12 @@
+
+   
 from django.shortcuts import render, get_object_or_404
 from django.contrib import messages
 
 from .models import UserProfile
 from .forms import UserProfileForm
 
+from checkout.models import Order
 
 def profile(request):
     """ Display the user's profile. """
@@ -26,4 +29,20 @@ def profile(request):
     }
 
     return render(request, template, context)
-    
+
+
+def order_history(request, order_number):
+    order = get_object_or_404(Order, order_number=order_number)
+
+    messages.info(request, (
+        f'This is a past confirmation for order number {order_number}. '
+        'A confirmation email was sent on the order date.'
+    ))
+
+    template = 'checkout/checkout_success.html'
+    context = {
+        'order': order,
+        'from_profile': True,
+    }
+
+    return render(request, template, context)   
