51109830595a287368dcbd284cd82216a402ab8f
nikimacm <nickymacm@gmail.com>
2022-05-27 05:08:48 +0000

    updated base template and mobile-top header

--- profiles/views.py
+++ profiles/views.py
@@ -1,5 +1,3 @@
-
-   
 from django.shortcuts import render, get_object_or_404
 from django.contrib import messages
 
@@ -17,8 +15,10 @@ def profile(request):
         if form.is_valid():
             form.save()
             messages.success(request, 'Profile updated successfully')
-
-    form = UserProfileForm(instance=profile)
+        else:
+            messages.error(request, 'Update failed. Please ensure the form is valid.')
+    else:
+        form = UserProfileForm(instance=profile)
     orders = profile.orders.all()
 
     template = 'profiles/profile.html'
@@ -45,4 +45,5 @@ def order_history(request, order_number):
         'from_profile': True,
     }
 
-    return render(request, template, context)   
+    return render(request, template, context)
+  
