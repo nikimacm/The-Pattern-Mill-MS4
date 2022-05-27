459f8118dc483bb83e401c74dab8ca6c9267ab87
nikimacm <nickymacm@gmail.com>
2022-05-27 08:40:35 +0000

    added migrationa

--- checkout/models.py
+++ checkout/models.py
@@ -17,7 +17,7 @@ class Order(models.Model):
     full_name = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(max_length=254, null=False, blank=False)
     phone_number = models.CharField(max_length=20, null=False, blank=False)
-    country = CountryField(blank_label='Country *', null=False, blank=False)
+    country = CountryField(max_length=100, blank_label='Country *', null=False, blank=False)
     postcode = models.CharField(max_length=20, null=True, blank=True)
     town_or_city = models.CharField(max_length=40, null=False, blank=False)
     street_address1 = models.CharField(max_length=80, null=False, blank=False)
@@ -43,7 +43,7 @@ class Order(models.Model):
         """
         self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum'] or 0
         if self.order_total < settings.ORDER_DISCOUNT_THRESHOLD:
-            self.delivery_cost = self.order_total * settings.ORDER_DISCOUNT_AMOUNT / 100
+            self.delivery_cost = self.order_total * settings.ORDER_DISCOUNT_AMOUNT / 10
         else:
             self.delivery_cost = 0
         self.grand_total = self.order_total + self.delivery_cost
